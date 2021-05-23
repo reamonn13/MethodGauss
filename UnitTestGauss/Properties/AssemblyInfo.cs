199aed869b12cb49f4f6db75973426dab7ab244f
reamonn13 <andreyperov_96@mail.ru>
2021-05-23 15:47:20 +0700

    first unit test

---
+++ UnitTestGauss/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется посредством следующего 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("UnitTestGauss")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("UnitTestGauss")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Задание значения false для атрибута ComVisible приведет к тому, что типы из этой сборки станут невидимыми 
+// для COM-компонентов.  Если к одному из типов этой сборки необходимо обращаться из 
+// модели COM, задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Если данный проект доступен для модели COM, следующий GUID используется в качестве идентификатора библиотеки типов
+[assembly: Guid("c1200580-82ae-475e-b99b-07f89a4a3e52")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [сборка: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestGauss/UnitTest1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using lab1_SLAU_MethodGauss;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.IO;
+using System.ComponentModel;
+using System.Linq;
+
+namespace UnitTestGauss
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestDeterminate()
+        {
+            double expertedValueDeterminate = 25;
+            string path = @"D:\Dlya kursovoj\lab1_SLAU_MethodGauss\a1.dat";
+            string[] readfile = File.ReadAllLines(path);  //чтение матрицы А из файла
+            string[] str = null;
+            int N = Convert.ToInt32(readfile[0]);
+            double[,] A = new double[N, N];
+            double[,] E = new double[N, N];
+            double[] B = new double[N];
+            double[] X = new double[N];
+            double[] R = new double[N];
+            int num_var;
+            for (int i = 1; i < readfile.Count(); i++)
+            {
+                str = readfile[i].Split(' ');
+                for (int j = 0; j < readfile.Count() - 1; j++)
+                {
+                    A[i - 1, j] = Convert.ToDouble(str[j]);  //запись матрицы А в двумерный массив
+                }
+            }
+
+            path = @"D:\Dlya kursovoj\lab1_SLAU_MethodGauss\b1.dat";
+            readfile = File.ReadAllLines(path);  //чтение матрицы B из файла
+            str = readfile[0].Split(' ');
+            for (int i = 0; i < N; i++)
+            {
+                B[i] = Convert.ToDouble(str[i]);  //запись матрицы B в одномерный массив
+            }
+
+            for (int i = 0; i < N; i++)  //заполнение единичной матрицы
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    if (i == j)
+                        E[i, j] = 1;
+
+                    else
+                        E[i, j] = 0;
+                }
+            }
+            double valueDeterminate = Program.MakeTriangleWithME(N, A, B, X, R, E);
+            Assert.AreEqual(expertedValueDeterminate, valueDeterminate);
+        }
+    }
+}
